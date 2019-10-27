9c3b5286b6e40846a8128e3175e17af53677d764
Valerio Cietto <siluxmedia@gmail.com>
2019-10-27 12:34:20 +0100

    fix
    view number is always an integer.

--- QuestionExport.js
+++ QuestionExport.js
@@ -1,11 +1,11 @@
 var number_of_questions = document.getElementsByClassName("partners_question_list_item").length;
 var i =0;
 var csv = "";
-for(i =0; i< number_of_questions; i++){
+for(i =0; i< 1000; i++){ //number_of_questions
   var element = document.getElementsByClassName("partners_question_list_item")[i];
   var question_text = "question text null";
   if(element.getElementsByClassName("ui_qtext_rendered_qtext")[0] != null){
-  	question_text = element.getElementsByClassName("ui_qtext_rendered_qtext")[0].innerHTML;
+    question_text = element.getElementsByClassName("ui_qtext_rendered_qtext")[0].innerHTML;
   }
   var question_earning = "0€";
   if(element.getElementsByClassName("earnings_amount")[0] != null){
@@ -17,6 +17,8 @@ for(i =0; i< number_of_questions; i++){
   }
   var external_traffic = "0";
   var question_views = element.getElementsByClassName("info")[1].innerHTML;
+  question_views = question_views.replace(".","");
+  question_views = question_views.replace(",","");
   if(element.getElementsByClassName("traffic_sources")[0] != null){
     external_traffic = element.getElementsByClassName("external_traffic")[0].innerText;
     external_traffic = external_traffic.replace(" % esterno", "");
@@ -25,8 +27,9 @@ for(i =0; i< number_of_questions; i++){
     external_traffic = "" + external_traffic_number;
   }
   if(!question_earning.includes("€")){
-	 //bad merged or excluded
-		question_earning = "€0.00"
-	}	
-	//console.log(question_text+"|"+question_earning);
-	csv += question_text+"|"+question_earning+"|"+external_traffic+"|"+question_views +"|"+question_number_of_answers +"\n";
+     //bad merged or excluded
+        question_earning = "€0.00"
+    }   
+    //console.log(question_text+"|"+question_earning);
+    csv += question_text+"\t"+question_earning+"\t"+external_traffic+"\t"+question_views +"\t"+question_number_of_answers +"\n";
+}
